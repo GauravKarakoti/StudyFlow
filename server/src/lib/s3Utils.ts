acda32ee8103a8415df145b9d32d444c6fc8564b
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-12-07 21:19:01 +0530

    Kal pta chlega

--- server/src/lib/s3Utils.ts
+++ server/src/lib/s3Utils.ts
@@ -1,4 +1,7 @@
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { 
+  S3Client, 
+  // GetObjectCommand
+} from '@aws-sdk/client-s3';
 // import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 export const s3 = new S3Client({
